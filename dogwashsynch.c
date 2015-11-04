29bdbd67e6877d323527b531fcac6389567a6854
Jordaen Graham <jhg257@minion16.usask.ca>
2015-11-04 16:20:52 -0600

    It works

--- dogwashsynch.c
+++ dogwashsynch.c
@@ -1,109 +1,54 @@
 #include <jmorecfg.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "dogwashsynch.h"
 #include <pthread.h>
 
 
-pthread_mutex_t DA_mutex, DB_mutex, bays_mutex, new_dog_mutex;
-/* Should we have a mutex where if there is an A dog in a bay, A holds
-the "can add" lock? So B can only be in there if the lock isnt held */
-int *DA_count, *DB_count, *bays_occupied, *num_bays;
+pthread_mutex_t DA_mutex, DB_mutex, bays_avail_mutex, bays_mutex;
+int *DA_count, *DB_count, *bays_avail, *num_bays;
 
-void *thread_enter(void * arg){
-  return NULL;
-}
-
-void increment_bays(){
-    pthread_mutex_lock(&bays_mutex);
-    bays_occupied++;
-    printf("\tIncrement Bays: %d\n", *bays_occupied);
-    pthread_mutex_unlock(&bays_mutex);
-}
-void decrement_bays(){
-    pthread_mutex_lock(&bays_mutex);
-    bays_occupied--;
-    printf("\tDecrement Bays: %d\n", *bays_occupied);
-    pthread_mutex_unlock(&bays_mutex);
-}
-int get_bays_occupied(){
-    int val;
-    pthread_mutex_lock(&bays_mutex);
-    val = *bays_occupied;
-    pthread_mutex_unlock(&bays_mutex);
-    return val;
-}
-boolean check_bays(){
-    boolean bool;
-    pthread_mutex_lock(&bays_mutex);
-/*    printf("Bays Occupied: %d\n", bays_occupied);*/
-    bool = (*bays_occupied >= *num_bays);
-    pthread_mutex_unlock(&bays_mutex);
-    return bool;
-}
-
-void increment_DA(){
+void DA_start(){
     pthread_mutex_lock(&DA_mutex);
-    printf("\tIncrement DA\n");
-    *DA_count++;
+    *DA_count = *DA_count + 1;
+    if (*DA_count == 1)
+        pthread_mutex_lock(&bays_mutex);
     pthread_mutex_unlock(&DA_mutex);
 }
-void decrement_DA(){
+
+void DB_start(){
+    pthread_mutex_lock(&DB_mutex);
+    *DB_count = *DB_count + 1;
+    if (*DB_count == 1)
+        pthread_mutex_lock(&bays_mutex);
+    pthread_mutex_unlock(&DB_mutex);
+}
+
+void DA_done(){
     pthread_mutex_lock(&DA_mutex);
-    printf("\tDecrement DA\n");
-    *DA_count--;
+    *DA_count = *DA_count - 1;
+    if (*DA_count == 0)
+        pthread_mutex_unlock(&bays_mutex);
     pthread_mutex_unlock(&DA_mutex);
 }
-int get_DA_count(){
-    int val;
-    pthread_mutex_lock(&DA_mutex);
-    val = *DA_count;
-    pthread_mutex_unlock(&DA_mutex);
-    return val;
-}
-boolean check_DA(){
-    boolean boolVar;
-    pthread_mutex_lock(&DB_mutex);
-    boolVar = (0==*DA_count);
-    pthread_mutex_unlock(&DB_mutex);
-    return boolVar;
-}
 
-void increment_DB(){
+void DB_done(){
     pthread_mutex_lock(&DB_mutex);
-    printf("\tIncrement DB\n");
-    *DB_count++;
+    *DB_count = *DB_count - 1;
+    if (*DB_count == 0)
+        pthread_mutex_unlock(&bays_mutex);
     pthread_mutex_unlock(&DB_mutex);
 }
-void decrement_DB(){
-    pthread_mutex_lock(&DB_mutex);
-    printf("\tDecrement DB\n");
-    *DB_count--;
-    pthread_mutex_unlock(&DB_mutex);
-}
-int get_DB_count(){
-    int val;
-    pthread_mutex_lock(&DB_mutex);
-    val = *DB_count;
-    pthread_mutex_unlock(&DB_mutex);
-    return val;
-}
-boolean check_DB(){
-    boolean boolVar;
-    pthread_mutex_lock(&DB_mutex);
-    boolVar = (0==DB_count);
-    pthread_mutex_unlock(&DB_mutex);
-    return boolVar;
-}
-
-
-
-
 
 
 
 int dogwash_init(int numbays) {
+    int rv;
+
+    srand(time(NULL));
+        
     DA_count = (int *)malloc(sizeof(int));
     if (0 == DA_count){
         printf("ERROR: Out of memory. Could not allocate DA_count.\n");
@@ -120,88 +65,126 @@ int dogwash_init(int numbays) {
     *DB_count = 0;
     
     
-    bays_occupied = (int *)malloc(sizeof(int));
-    if (0 == bays_occupied){
-        printf("ERROR: Out of memory. Could not allocate bays_occupied.\n");
+    bays_avail = (int *)malloc(sizeof(int));
+    if (0 == bays_avail){
+        printf("ERROR: Out of memory. Could not allocate bays_avail.\n");
         return -1;
     }
-    *bays_occupied = 0;
+    *bays_avail = numbays;
     
     num_bays = (int *)malloc(sizeof(int));
-    if (0 == DB_count){
+    if (0 == num_bays){
         printf("ERROR: Out of memory. Could not allocate num_bays.\n");
         return -1;
     }
     *num_bays = numbays;
     
+    rv = pthread_mutex_init(&bays_mutex, NULL);
+    if (0 != rv){
+        printf("ERROR: Out of memory. Could not allocate bays_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_init(&bays_avail_mutex, NULL);
+    if (0 != rv){
+        printf("ERROR: Out of memory. Could not allocate bays_avail_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_init(&DA_mutex, NULL);
+    if (0 != rv){
+        printf("ERROR: Out of memory. Could not allocate DA_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_init(&DB_mutex, NULL);
+    if (0 != rv){
+        printf("ERROR: Out of memory. Could not allocate DB_mutex.\n");
+        return -1;
+    }
+
     return 0;
 }
 
-
-
-void *newdog_init(void *arg){
-    newdog((dogtype) *arg);
-
-
-
-int newdog(dogtype dog){
-    pthread_mutex_lock(&new_dog_mutex);
-    printf("Dog, %s Arrived\n", DA == dog ? "A" : DB == dog ? "B" : "O");
-    if (DA == dog) {
-        while (!check_DB()) {printf("While DB: %d\n", get_DB_count());}
-        increment_DA();
-    }
-    if (DB == dog) {
-        while (!check_DA()) {printf("While DA: %d\n", get_DA_count());}
-        increment_DB();
-    }
-
-    while (check_bays()) ;
-    increment_bays();
-    printf("Bays Occupied: %d\n", get_bays_occupied());
-
-    printf("Dog, %s_%d Washing in bay %d\n", DA == dog ? "A" : DB == dog ? "B" : "O", DA == dog ? get_DA_count() : DB == dog ? get_DB_count() : 0, get_bays_occupied());
-
-    pthread_mutex_unlock(&new_dog_mutex);
+void *dog(void *arg){
+    int num = newdog((dogtype) arg);
+    sleep(1);
+    num = dogdone((dogtype) arg);
+    return NULL;
 }
 
+int newdog(dogtype dog){
+    printf("Dog, %s Arrived\n", DA == dog ? "A" : DB == dog ? "B" : "O");
 
+    if (DA == dog)
+        DA_start();
+    else if (DB == dog)
+        DB_start();
 
+    pthread_mutex_lock(&bays_avail_mutex);
+    while (0 == *bays_avail){
+        pthread_mutex_unlock(&bays_avail_mutex);
+        pthread_mutex_lock(&bays_avail_mutex);
+    }
+    if (0 > *bays_avail)
+        printf("Negative bays avail");
+    *bays_avail = *bays_avail - 1;
+    pthread_mutex_unlock(&bays_avail_mutex);
+        
+    printf("Dog, %s_%d washing. Bay Remaining: %d\n\n", DA == dog ? "A" : DB == dog ? "B" : "O", DA == dog ? *DA_count : DB == dog ? *DB_count : 0, *bays_avail);
 
-
+    return 0;
+}
 
 int dogdone(dogtype dog) {
-    sleep(1);
-    printf("Dog: %s Done\n", DA == dog ? "A" : DB == dog ? "B" : "O");
+    printf("Dog: %s Done\n\n", DA == dog ? "A" : DB == dog ? "B" : "O");
+
+    pthread_mutex_lock(&bays_avail_mutex);
+    *bays_avail = *bays_avail + 1;
+    pthread_mutex_unlock(&bays_avail_mutex);    
+
     if ( DA == dog ){
-      decrement_DA();
+      DA_done();
     }else if ( DB == dog ){
-      decrement_DB();
+      DB_done();
     }
 
-    printf("DA: %d, DB: %d\n", get_DA_count(), get_DB_count());
-    decrement_bays();
     pthread_exit(NULL);
     return 0;
 }
 
-
-
-
-
-
 int dogwash_done() {
+    int rv;
+    
     free(DA_count);
     DA_count = NULL;
     
     free(DB_count);
     DB_count = NULL;
     
-    free(bays_occupied);
-    bays_occupied = NULL;
+    free(bays_avail);
+    bays_avail = NULL;
 
     free(num_bays);
     num_bays = NULL;
+
+    rv = pthread_mutex_destroy(&bays_mutex);
+    if (0 != rv){
+        printf("Failed to destroy bays_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_destroy(&bays_avail_mutex);
+    if (0 != rv){
+        printf("Failed to destroy bays_avail_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_destroy(&DA_mutex);
+    if (0 != rv){
+        printf("Failed to destroy DA_mutex.\n");
+        return -1;
+    }
+    rv = pthread_mutex_destroy(&DB_mutex);
+    if (0 != rv){
+        printf("Failed to destroy DB_mutex.\n");
+        return -1;
+    }
     
     return 0;
 }
@@ -214,64 +197,64 @@ int main(int argc, char **argv) {
 
     
     int rc, i, numBays;
-    const int numDogs = 12;
-    numBays = 5;
+    int numDogs;
     pthread_t dogs[12];
-    dogtype *array;
+    int r;
+    dogtype doggy;
 
-    printf("Hello, World!\n");
-    printf("Numbays: %d\n", numBays);
+    numDogs = 12;
+    numBays = 5;
+
+    printf("\tWelcome to our dog wash! \nWe currently have %d bays available today\n\n", numBays);
     
     if(0 != dogwash_init(numBays)){
       printf("Error in initializing dogwash with %i bays.\n", numBays);
       return -1;
     }
-
     
-    array = malloc(numDogs*sizeof(dogtype));
-    array[0] = DB;
-    array[1] = DB;
-    array[2] = DB;
-    array[3] = DB;
-    array[4] = DB;
-    array[5] = DB;
-    array[6] = DB;
 
-    array[7] = DA;
-    array[8] = DA;
-    array[9] = DA;
-    array[10] = DA;
-    array[11] = DA;
-
-    pthread_mutex_init(&bays_mutex, NULL);
-    pthread_mutex_init(&new_dog_mutex, NULL);
-    pthread_mutex_init(&DA_mutex, NULL);
-    pthread_mutex_init(&DB_mutex, NULL);
 
     for (i = 0; i < numDogs; i++) {
-        rc = pthread_create(&dogs[i], NULL, newdog, (void *)array[i]);
+        r = rand();
+        if (0 == r % 3)
+            doggy = DA;
+        if (1 == r % 3)
+            doggy = DB;
+        if (2 == r % 3)
+            doggy = DO;
+        
+        rc = pthread_create(&dogs[i], NULL, dog, (void *)doggy);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             return -1;
         }
-        printf("completed pthread_create #%i\n\n", i);
     }
     for (i = 0; i < numDogs; i++){
         rc = pthread_join(dogs[i], NULL);
-        printf("Did we get to pthread_join?\n");
         if (rc){
             printf("ERROR; return code from pthread_join() is %d\n", rc);
             return -1;
         }
-        dogdone(array[i]);
     }
 
-    pthread_mutex_destroy(&bays_mutex);
-    pthread_mutex_destroy(&new_dog_mutex);
-    pthread_mutex_destroy(&DA_mutex);
-    pthread_mutex_destroy(&DB_mutex);
-
-    printf("Done creating threads\n");
+    printf("Done Washing Dogs\n");
     /* Last thing that main() should do */
-    pthread_exit(NULL);
-}
+    return dogwash_done();
+    }
+    
+    
+    
+    
+/*
+
+
+
+
+
+
+
+
+
+
+
+*/
